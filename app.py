d79f1b1f6934a2cfdb630acd546141d4544b2e71
Bon Crowder @MathFour <mathfour.com@gmail.com>
2018-12-07 17:32:12 -0600

    database may work on pythonanywhere

--- app.py
+++ app.py
@@ -24,15 +24,18 @@ class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(4096))
 
-comments = []
+# comments = []
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template('comments_page.html', comments=comments)
+        return render_template('comments_page.html', comments=Comment.query.all())
 
     # elif request.method == 'POST':
-    comments.append(request.form["contents"])
+    # comments.append(request.form["contents"])
+    comment = Comment(content=request.form["contents"])
+    db.session.add(comment)
+    db.session.commit()
     return redirect(url_for('index'))
 
 
