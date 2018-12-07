f525048794d56fbed250a4de7b42006644131985
Bon Crowder @MathFour <mathfour.com@gmail.com>
2018-12-07 17:20:01 -0600

    mysql with sqlalchemy is there but not yet working

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
 from flask import Flask, redirect, request, render_template, url_for
 from processing import do_calculation
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 # app.config["DEBUG"] = True
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="MathFour",
+    password="bB88!**W6W9ccb",
+    hostname="MathFour.mysql.pythonanywhere-services.com",
+    databasename="MathFour$comments",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class Comment(db.Model):
+
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
+
 comments = []
 
 @app.route("/", methods=["GET", "POST"])
